f2a6ab1eb1e5abf8e8a57cc5388447308a5a95e2
Mug3n96 <heinrich.root@outlook.de>
2021-02-25 03:00:15 +0100

    i need some sleep

--- public/static/views/Home.js
+++ public/static/views/Home.js
@@ -12,9 +12,10 @@ export default class extends AbstractView {
         <header class="article-header__container">
           <h1 class="article-header__title">Welcome!</h1>
           <h3 class="article-header__subtitle">[EU|GER] Dio's Mansion</h3>
-          <h5 class="article-header__subtitle">IP: 173.212.227.88</h5>
+          <h5 class="article-header__subtitle">IP: 173.212.227.88:28015</h5>
         </header>
-        <p>We will envolve players as much as we can into the decisions. If you dont like smth about the serversetting or you know a good plugin which you would love to see on this server, just tell us that. If we, or the players have nothing to say against it, we will include your idea into the server.</p>
+        <p>If you are looking for a fair and toxic free server, you came to the right place! We will envolve players as much as we can into the decisions.</p> 
+        <p>If you dont like smth about the serversetting or you know a good plugin which you would love to see on this server, just tell us that. If we, or the players have nothing to say against it, we will include your idea into the server.</p>
         <ul>
           <li>server exists since 13.02.2021</li>
           <li>Wipe Day: 13.02.2021</li>
--- public/static/views/Plugins.js
+++ public/static/views/Plugins.js
@@ -8,7 +8,65 @@ export default class extends AbstractView {
 
   async getHtml() {
     return `
-      <h1>Plugins</h1>
+      <h1 class="title-plugins">Plugins</h1>
+      <div class="articles-container plugin">
+        <article class="article plugin">
+          <header class="article-header__container">
+            <h3 class="article-header__title">Anti Offline Raid</h3>
+            <h5>Plugin which makes offline raiding much harder.</h5>
+          </header>
+          <a href="plugins/antiofflineraid" 
+            class="article-button plugin">read more</a>
+        </article>
+        <article class="article plugin">
+          <header class="article-header__container">
+            <h3 class="article-header__title">Remover Tool</h3>
+            <h5>Removes with "/remove" commando structures.</h5>
+          </header>
+          <a href="plugins/removertool" 
+            class="article-button plugin">read more</a>
+        </article>
+        <article class="article plugin">
+          <header class="article-header__container">
+            <h3 class="article-header__title">Info Panel</h3>
+            <h5>Removes with "/remove" commando structures.</h5>
+          </header>
+          <a href="plugins/infopanel"
+            class="article-button plugin">read more</a>
+        </article>
+        <article class="article plugin">
+          <header class="article-header__container">
+            <h3 class="article-header__title">Dangerous Treasures</h3>
+            <h5>Removes with "/remove" commando structures.</h5>
+          </header>
+          <a href="plugins/dangeroustreasures"
+            class="article-button plugin">read more</a>
+        </article>
+        <article class="article plugin">
+          <header class="article-header__container">
+            <h3 class="article-header__title">Visual Cupboard</h3>
+            <h5>Removes with "/remove" commando structures.</h5>
+          </header>
+          <a href="plugins/visualcupboard"
+            class="article-button plugin">read more</a>
+        </article>
+        <article class="article plugin">
+          <header class="article-header__container">
+            <h3 class="article-header__title">Stack Size Controller</h3>
+            <h5>Removes with "/remove" commando structures.</h5>
+          </header>
+          <a href="plugins/stacksizecontroller"
+            class="article-button plugin">read more</a>
+        </article>
+        <article class="article plugin">
+          <header class="article-header__container">
+            <h3 class="article-header__title">Gather Manager</h3>
+            <h5>Removes with "/remove" commando structures.</h5>
+          </header>
+          <a href="gathermanager"
+            class="article-button plugin">read more</a>
+        </article>
+      </div>
     `; 
   }
 }
